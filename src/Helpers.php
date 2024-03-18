249ca4635348195fabfdc5979a4c63401ecfb796
yyqsg888 <61581602+yyqsg888@users.noreply.github.com>
2024-03-18 19:09:44 +0800

    Implement caching

--- src/Helpers.php
+++ src/Helpers.php
@@ -2,19 +2,40 @@
 
 function minify($file)
 {
-    $path = resource_path($file);
-    if (!file_exists($path)) {
-        throw new \Exception('File not found');
-    }
+    return Cache::rememberForever('minify_path_' . $file, function () use ($file) {
+        $path = resource_path($file);
+        if (!file_exists($path)) {
+            throw new \Exception('File not found');
+        }
 
-    $path .= '?v=' . hash_file('sha256', $path);
+        $hash = hash_file('sha256', $path);
+        \Log::warning('File hashed: ' . $path);
 
-    \Log::warning('File hashed: ' . $path);
+        // remove slash or backslash from the beginning of the file path
+        $file = ltrim($file, '/\\');
 
-    // remove slash or backslash from the beginning of the file path
-    $file = ltrim($file, '/\\');
+        $path = '_minify/' . $file;
+        $path .= '?v=' . $hash;
 
-    $path = '_minify/' . $file;
+        return $path;
+    });
+}
 
-    return $path;
+function minify_noversioning($file)
+{
+    return Cache::rememberForever('minify_path_' . $file, function () use ($file) {
+        $path = resource_path($file);
+        if (!file_exists($path)) {
+            throw new \Exception('File not found');
+        }
+
+        \Log::warning('File no versioning: ' . $path);
+
+        // remove slash or backslash from the beginning of the file path
+        $file = ltrim($file, '/\\');
+
+        $path = '_minify/' . $file;
+
+        return $path;
+    });
 }
